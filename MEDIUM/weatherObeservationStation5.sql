b4af023b8fa58d232558d48ed4bde561884d087f
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-12 13:56:49 +0530

    Added optimised query

--- MEDIUM/weatherObeservationStation5.sql
+++ MEDIUM/weatherObeservationStation5.sql
@@ -17,3 +17,24 @@ SELECT city, LENGTH(city)
 WHERE LENGTH(city) = (
                       SELECT MAX(LENGTH(city)) FROM STATION 
                       )ORDER BY city LIMIT 0,1;             
+
+-->> Alternate solution using UNION in a single query  :
+(
+  SELECT s1.city, LENGTH(s1.city) AS city_length
+  FROM STATION s1
+  WHERE LENGTH(s1.city) = (SELECT MIN(LENGTH(s2.city)) FROM STATION s2)
+  ORDER BY s1.city
+  LIMIT 1
+)
+UNION
+(
+  SELECT s1.city, LENGTH(s1.city) AS city_length
+  FROM STATION s1
+  WHERE LENGTH(city) = (SELECT MAX(LENGTH(s2.city)) FROM STATION s2 ) 
+  ORDER BY s1.city DESC
+  LIMIT 1
+);
+
+--> 1st Query will fetch us the min city and its length
+--> 2nd Query will fetch us the min city and its length
+--> Finally, UNION will combine the result of both.
